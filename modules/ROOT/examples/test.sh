77785edab72548fabe4048be8c9e1001cc525fdc
Lorina Poland <lorinapoland@gmail.com>
2021-06-09 21:18:16 -0700

    really these are the new Use the DevOps API pages and examples

---
+++ modules/ROOT/examples/test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# SET THE BEARER TOKEN FOR ALL OTHER COMMANDS
+
+#export BEARER_TOKEN=AstraCS:cFbZGeOumxfmwqlQiDGRspUz:4d9175e2623e5017792b6c396ba2b3b48a46b5519cc8c1a5a92af3791e5f23f9
+BEARER_TOKEN=AstraCS:cFbZGeOumxfmwqlQiDGRspUz:4d9175e2623e5017792b6c396ba2b3b48a46b5519cc8c1a5a92af3791e5f23f9
+
+
+# SET VARIABLES
+base_url=https://api.dev.streaming.datastax.com
+base_api=/v2/streaming
+tenant_name=llp-llp-testcreater
+tenant_name_with_topics=test-llp
+topic_name=blah
+cluster_name=astradev-aws
+
+for FILE in *;
+ do
+    if [[ "$FILE" != "test"* ]]
+    then
+      gsed "s#{base_url}#$base_url#; s#{base_api}#$base_api#; s#{tenant_name}#$tenant_name#; s#{tenant_name_with_topics}#$tenant_name_with_topics#; s#{topic_name}#$topic_name#; s#{cluster_name}#$cluster_name#; s#{bearer_token}#$BEARER_TOKEN#;" $FILE > $FILE.tmp;
+      chmod 755 $FILE.tmp;
+    fi
+done
+
+echo -e "\nGet all clusters\n"
+./get_clusters.sh.tmp | jq
+
+echo -e "\nGet all providers\n"
+./get_providers.sh.tmp | jq
+
+echo -e "\nGet all tenants\n"
+./get_tenants.sh.tmp | jq
+
+echo -e "\nCheck tenant exists\n"
+./check_tenant_exists.sh.tmp
+
+echo -e "\nCreate tenant\n"
+./create_tenant.sh.tmp | jq
+
+echo -e "\nGet tenant limits\n"
+./get_tenant_limits.sh.tmp | jq
+
+echo -e "\nGet all tenants again, now that new tenant is created\n"
+./get_tenants.sh.tmp | jq
+
+echo -e "\nDelete tenant hard\n"
+./delete_tenant_hard.sh.tmp
+
+#echo -e "\nDelete tenant soft\n"
+#./delete_tenant_soft.sh.tmp
+
+# CLEAN UP tmp files
+rm *.tmp; 
